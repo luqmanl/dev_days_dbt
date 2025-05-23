e3779f0b62926ba9092779e47eeb311e405e3785
Luqman Liaquat <luqman@etleap.com>
2025-05-23 11:49:36 +0100

    Add name to tables

--- models/marts/logins_by_user.sql
+++ models/marts/logins_by_user.sql
@@ -1,4 +1,4 @@
 SELECT u.id AS user_id, COUNT(e.external_id) AS logins 
-FROM {{source('LUQMAN', 'USER_INFO')}} u, {{source('LUQMAN', 'WEB_EVENTS')}} e
+FROM {{source('LUQMAN', 'LUQMAN_USER_INFO')}} u, {{source('LUQMAN', 'LUQMAN_WEB_EVENTS')}} e
 WHERE u.external_id = e.external_id
 GROUP BY u.id
--- models/marts/spend_per_user.sql
+++ models/marts/spend_per_user.sql
@@ -1,5 +1,5 @@
 SELECT p.user_id, SUM(i.price) AS spend
   FROM {{source('LUQMAN', 'PURCHASE')}} p
-    INNER JOIN {{source('LUQMAN', 'LINE_ITEM')}} li ON li.purchase_id = p.id
-    INNER JOIN {{source('LUQMAN', 'ITEM')}} i ON i.item_id = li.item_id
+    INNER JOIN {{source('LUQMAN', 'LUQMAN_LINE_ITEM')}} li ON li.purchase_id = p.id
+    INNER JOIN {{source('LUQMAN', 'LUQMAN_ITEM')}} i ON i.item_id = li.item_id
   GROUP BY p.user_id
